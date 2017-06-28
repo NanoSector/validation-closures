c4c4fb7a28a18c29216fe1af9309e8a53c5e70f6
Yoshi2889 <rick.2889@gmail.com>
2017-06-28 18:03:21 +0200

    Add range closures
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

---
+++ src/Ranges.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Copyright 2017 NanoSector
+ *
+ * You should have received a copy of the MIT license with the project.
+ * See the LICENSE file for more information.
+ */
+
+namespace ValidationClosures;
+
+
+class Ranges
+{
+	/**
+	 * @param int $minimumLength
+	 * @param int $maximumLength
+	 *
+	 * @return \Closure
+	 */
+	public static function stringWithLength(int $minimumLength, int $maximumLength): \Closure
+	{
+		if ($maximumLength < 0 || $maximumLength < 1)
+			throw new \InvalidArgumentException('Minimum length cannot be below 0, maximum length cannot be below 1');
+
+		return function (string $value) use ($minimumLength, $maximumLength)
+		{
+			return strlen($value) >= $minimumLength && strlen($value) <= $maximumLength;
+		};
+	}
+
+	/**
+	 * @param int $minimum
+	 * @param int $maximum
+	 *
+	 * @return \Closure
+	 */
+	public static function intBetween(int $minimum, int $maximum): \Closure
+	{
+		return function (int $value) use ($minimum, $maximum)
+		{
+			return $value >= $minimum && $value <= $maximum;
+		};
+	}
+
+	/**
+	 * @param array ...$allowedValues
+	 *
+	 * @return \Closure
+	 */
+	public static function enum(...$allowedValues): \Closure
+	{
+		return function ($value) use ($allowedValues)
+		{
+			if (is_object($value))
+			{
+				foreach ($allowedValues as $allowedValue)
+					if (is_a($value, $allowedValue))
+						return true;
+
+				return false;
+			}
+
+			return in_array(gettype($value), $allowedValues);
+		};
+	}
+
+	/**
+	 * @param array ...$allowedValues
+	 *
+	 * @return \Closure
+	 */
+	public static function stringOneOf(...$allowedValues): \Closure
+	{
+		return function (string $value) use ($allowedValues)
+		{
+			return in_array($value, $allowedValues);
+		};
+	}
+}
