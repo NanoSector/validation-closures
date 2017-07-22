3481b7e1da07bf3a9a54055ba19b59aa90c02163
Yoshi2889 <rick.2889@gmail.com>
2017-07-22 20:04:43 +0200

    Add Reflection class
    All methods which exist in a ReflectionClass can be converted into a closure using this class.
    
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

---
+++ src/Reflection.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Copyright 2017 NanoSector
+ *
+ * You should have received a copy of the MIT license with the project.
+ * See the LICENSE file for more information.
+ */
+
+namespace ValidationClosures;
+
+/**
+ * Class Reflection
+ * @package ValidationClosures
+ *
+ * Taken from \ReflectionClass; all methods which could be used properly as a validation closure.
+ * Nothing stops you from using the other methods though, but they'll be less useful as a validation mechanism.
+ * @method static bool hasConstant(string $name)
+ * @method static bool hasMethod(string $name)
+ * @method static bool hasProperty(string $name)
+ * @method static bool implementsInterface(string $interface)
+ * @method static bool inNamespace()
+ * @method static bool isAbstract()
+ * @method static bool isAnonymous()
+ * @method static bool isCloneable()
+ * @method static bool isFinal()
+ * @method static bool isInstance(object $object)
+ * @method static bool isInstantiable()
+ * @method static bool isInterface()
+ * @method static bool isInternal()
+ * @method static bool isIterateable()
+ * @method static bool isSubclassOf(string $class)
+ * @method static bool isTrait()
+ * @method static bool isUserDefined()
+ *
+ */
+class Reflection
+{
+	/**
+	 * @param string $class
+	 *
+	 * @return \ReflectionClass
+	 */
+	public static function createReflectionObject(string $class): \ReflectionClass
+	{
+		if (!class_exists($class))
+			throw new \InvalidArgumentException('The given class does not exist');
+
+		return new \ReflectionClass($class);
+	}
+
+	/**
+	 * @param string $method
+	 * @param array $arguments
+	 *
+	 * @return \Closure
+	 */
+	public static function __callStatic(string $method, array $arguments): \Closure
+	{
+		if (!method_exists(\ReflectionClass::class, $method))
+			throw new \InvalidArgumentException('Cannot create closure from method ReflectionClass::' . $method . ', it does not exist');
+
+		return function ($value) use ($method, $arguments)
+		{
+			if (!Types::string()($value))
+				return false;
+
+			$reflection = static::createReflectionObject($value);
+
+			return call_user_func_array([$reflection, $method], $arguments);
+		};
+	}
+}
---
+++ tests/ReflectionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Copyright 2017 NanoSector
+ *
+ * You should have received a copy of the MIT license with the project.
+ * See the LICENSE file for more information.
+ */
+
+use ValidationClosures\Reflection;
+use PHPUnit\Framework\TestCase;
+
+class ReflectionTest extends TestCase
+{
+	public function testCreateReflectionObject()
+	{
+		$expectedReflection = new ReflectionClass(stdClass::class);
+		$reflection = Reflection::createReflectionObject(stdClass::class);
+		self::assertEquals($expectedReflection, $reflection);
+
+		$this->expectException(\InvalidArgumentException::class);
+		Reflection::createReflectionObject('blablabla\nonexistingclass');
+	}
+
+	public function testCallStaticMagicMethod()
+	{
+		self::assertFalse(Reflection::isInterface()(stdClass::class));
+		self::assertTrue(Reflection::isInstance(new stdClass())(stdClass::class));
+
+		$this->expectException(\InvalidArgumentException::class);
+		Reflection::nonExistingMethod();
+	}
+}
